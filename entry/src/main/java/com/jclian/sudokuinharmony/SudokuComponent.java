7cbda91f7a3869c4aa4944b3ac31ecef53302483
lianjc <lianjinchu@@gznewsoft.com>
2021-06-03 17:06:20 +0800

    backup

--- entry/src/main/java/com/jclian/sudokuinharmony/SudokuComponent.java
+++ entry/src/main/java/com/jclian/sudokuinharmony/SudokuComponent.java
@@ -1,13 +1,18 @@
 package com.jclian.sudokuinharmony;
 
-import ohos.agp.colors.Color;
 import ohos.agp.components.Component;
 import ohos.agp.components.DragEvent;
 import ohos.agp.render.Canvas;
 import ohos.agp.render.Paint;
 import ohos.agp.render.Path;
 import ohos.agp.render.PathEffect;
+import ohos.agp.utils.Color;
+import ohos.agp.utils.RectFloat;
+import ohos.agp.utils.TextAlignment;
 import ohos.app.Context;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 public class SudokuComponent extends Component implements Component.DrawTask {
     private float yBtmFun = -1f;
@@ -55,9 +60,7 @@ public class SudokuComponent extends Component implements Component.DrawTask {
         dashPaint.setStrokeWidth(2f);
         dashPaint.setColor((new ohos.agp.utils.Color(0x2f2e2b)));
         dashPaint.setStyle(Paint.Style.STROKE_STYLE);
-        // todo 点类型
-//        dashPaint.setPathEffect(new PathEffect(floatArrayOf(wBlock * 0.6f, wBlock * 0.4f), wBlock * -0.2f));
-        dashPaint.setPathEffect(new PathEffect(null , wBlock * 0.6f, wBlock * 0.4f, PathEffect.Style.MORPH_STYLE));
+        dashPaint.setPathEffect(new PathEffect(new float[]{wBlock * 0.6f, wBlock * 0.4f}, wBlock * -0.2f));
 
 
         for (int i = 0; i <= 8; i++) {
@@ -80,5 +83,46 @@ public class SudokuComponent extends Component implements Component.DrawTask {
 
         }
 
+    }
+
+    private void drawNumPad(Canvas canvas) {
+        paint.reset();
+        //设定字体大小和对齐方式
+        paint.setTextSize((int) (getWidth() / 5f * 0.6f));
+        paint.setTextAlign(TextAlignment.CENTER);
+        // 数字按钮区域
+        for (int num = 1; num <= 10; num++) {
+            String text;
+            if (num == 10) {
+                text = "X";
+            } else {
+                text = String.valueOf(num);
+            }
+
+            float left = (num - 1) % 5 / 5f * getWidth();
+            float top = ((num - 1) / 5) * getWidth() / 5f + getWidth();
+            float textHeight = paint.descent() - paint.ascent();
+            float textOffset = textHeight / 2 - paint.descent();
+            RectFloat bounds = new RectFloat(left, top, left + wMenu, top + wMenu);
+            if (menuNum == num) {
+                paint.setColor(new Color(0x9c915d));
+                paint.setStyle(Paint.Style.FILLANDSTROKE_STYLE);
+                canvas.drawCircle(bounds.getCenter().getPointX(), bounds.getCenter().getPointY(), menuCircleRadius, paint);
+                paint.setColor(new Color(0xFFFFFF));
+                canvas.drawText(paint, text, bounds.getCenter().getPointX(), bounds.getCenter().getPointY() + textOffset);
+            } else {
+                paint.setColor(Color.WHITE);
+                paint.setStyle(Paint.Style.STROKE_STYLE);
+                canvas.drawCircle(bounds.getCenter().getPointX(), bounds.getCenter().getPointY(), menuCircleRadius, paint);
+                paint.setStyle(Paint.Style.FILLANDSTROKE_STYLE);
+                canvas.drawText(
+                        paint,
+                        text,
+                        bounds.getCenter().getPointX(),
+                        bounds.getCenter().getPointY() + textOffset
+                );
+            }
+
+        }
     }
 }
